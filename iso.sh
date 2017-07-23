c8968fa61e955224c5c0715467a9aabc60609c34
alyptik <alyptik@protonmail.com>
2017-07-23 12:59:07 -1000

    qemu.sh: update qemu flags

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/i686_elf_os.kernel isodir/boot/i686_elf_os.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "i686_elf_os" {
+	multiboot /boot/i686_elf_os.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o i686_elf_os.iso isodir
--- qemu.sh
+++ qemu.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+
 set -e
+# shellcheck disable=SC1091
 . ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom myos.iso
+qemu-system-"$(./target-triplet-to-arch.sh "$HOST")" \
+	-vga virtio -boot order=d -enable-kvm -m 2048 -smp 2 -soundhw hda \
+	-machine type=pc,accel=kvm -show-cursor -usbdevice tablet \
+	-cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time \
+	-net nic -net bridge,br=virbr0 -monitor stdio -monitor pty \
+	-cdrom i686_elf_os.iso
